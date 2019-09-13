90d4de9e1b4d08631bcafbce0b9c42c43e57a71f
Marvinsky <marvin.abisrror@gmail.com>
2019-09-13 18:38:47 -0500

    Fixing issues

--- include/format.h
+++ include/format.h
@@ -2,9 +2,11 @@
 #define FORMAT_H
 
 #include <string>
+using namespace std;
 
 namespace Format {
 std::string ElapsedTime(long times);  // TODO: See src/format.cpp
+std::string Pad(string s, char c);
+std::string ElapsedTimePad(long times);
 };                                    // namespace Format
-
 #endif
--- src/format.cpp
+++ src/format.cpp
@@ -7,10 +7,15 @@
 using std::string;
 using std::to_string;
 
+
+std::string Format::Pad(string s, char c) {
+    s.insert(s.begin(), 2 - s.size(), c);
+    return s;
+}
+
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long seconds) {
     long h, m, s;
     h = seconds/3600;
@@ -23,4 +28,12 @@ string Format::ElapsedTime(long seconds) {
     stream<<((h<10)?"0"+to_string(h):to_string(h)) <<":"<< ((m<10)?"0"+to_string(m):to_string(m)) <<":"<< ((s<10)?"0"+to_string(s):to_string(s)) ;
 
     return stream.str(); 
+}
+
+std::string Format::ElapsedTimePad(long seconds) {
+    long h, m, s;
+    h = seconds/(60*60);
+    m = (seconds/60)%60;
+    s = seconds%60;
+    return std::string(Format::Pad(to_string(h), '0') + ":" + Format::Pad(to_string(m), '0') + ":" + Format::Pad(to_string(s), '0'));
 }
